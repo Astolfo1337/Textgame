acf9d1ac320f2858a0c544676475d1f085a8910d
Astolfo1337 <bn@nemetzguss.at>
2018-10-21 17:33:33 +0200

    expanded main methods + wrote down new ideas

--- GameMNG.py
+++ GameMNG.py
@@ -3,7 +3,13 @@ from obj.Roomcell import RoomCell
 from obj.Map import Map
 import IOLoader
 
+def test():
+	pass
+
 def start():
+	print("Textgame\n")
+	print("Good day, my friend! Would you like to LOAD a game or start aNEW?")
+	getAction = input("Your descision: ")
 	map = IOLoader.loadMap()
 	#print(map.coordinates[1][1].content)
 	
@@ -11,7 +17,27 @@ def start():
 	#print (char.health)
 	#char.addHealth(-20)
 	#print (char.health)
+	mainProcess(pc, map)
 	test()
 	
-def test():
+def action(pc, currentMap, key):
+	return pc, currentMap
+	
+def movePC(pc, currentMap):
 	pass
+
+def refreshMap(pc, currentMap):
+	pass
+	
+def getInput():
+	return input ('Your action: ')
+	
+def refreshUI(pc, currentMap):
+	pass
+	
+def mainProcess(pc, currentMap):
+	active = True
+	while(active):
+		key = getInput()
+		pc, currentMap = action(pc, currentMap,key)
+		refreshUI(pc, currentMap)
